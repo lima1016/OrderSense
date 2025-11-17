52d63080a705750c0221b13602bf5d4ba74198c4
YoulimKim <kimyoulim1016@gmail.com>
2025-11-17 23:49:29 +0900

    setting: 환경 설정

---
+++ action-executor-service/src/main/java/com/lima/actionexecutorservice/ActionExecutorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lima.actionexecutorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionExecutorServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ActionExecutorServiceApplication.class, args);
+  }
+
+}
---
+++ action-executor-service/src/test/java/com/lima/actionexecutorservice/ActionExecutorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lima.actionexecutorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionExecutorServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
